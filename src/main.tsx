1b750330d557495e9a5c52694ad68b78ca6c2e25
duvanflor <duvan.flor@netto.com.co>
2024-01-22 07:15:03 -0500

    Fix: Creation component card character

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import 'react-alice-carousel/lib/alice-carousel.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
